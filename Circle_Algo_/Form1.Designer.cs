fcdd30d22db87eb617638ad14ab34dd81ecc642e
Touraj Ostovari <tornet656@gmail.com>
2019-10-25 18:01:57 +0330

    Add project files.

---
+++ Circle_Algo_/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Circle_Algo_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.R_updown = new System.Windows.Forms.NumericUpDown();
+            this.PosX_updown = new System.Windows.Forms.NumericUpDown();
+            this.PosY_updown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.R_updown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PosX_updown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PosY_updown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.groupBox2.Controls.Add(this.PosY_updown);
+            this.groupBox2.Controls.Add(this.PosX_updown);
+            this.groupBox2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(566, 157);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Position:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.groupBox3.Controls.Add(this.R_updown);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft New Tai Lue", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(566, 281);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "R:";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Javanese Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(566, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Draw Circle ...";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // R_updown
+            // 
+            this.R_updown.Location = new System.Drawing.Point(7, 38);
+            this.R_updown.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.R_updown.Name = "R_updown";
+            this.R_updown.Size = new System.Drawing.Size(187, 28);
+            this.R_updown.TabIndex = 0;
+            // 
+            // PosX_updown
+            // 
+            this.PosX_updown.Location = new System.Drawing.Point(7, 23);
+            this.PosX_updown.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.PosX_updown.Name = "PosX_updown";
+            this.PosX_updown.Size = new System.Drawing.Size(187, 23);
+            this.PosX_updown.TabIndex = 0;
+            // 
+            // PosY_updown
+            // 
+            this.PosY_updown.Location = new System.Drawing.Point(7, 65);
+            this.PosY_updown.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.PosY_updown.Name = "PosY_updown";
+            this.PosY_updown.Size = new System.Drawing.Size(187, 23);
+            this.PosY_updown.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(544, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Y";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(544, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "X";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(513, 324);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Radius";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Self Designed Circle Drawing Algorithm || Mr.Touraj Ostovari || 2019 Oct 25";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.R_updown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PosX_updown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PosY_updown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown R_updown;
+        private System.Windows.Forms.NumericUpDown PosY_updown;
+        private System.Windows.Forms.NumericUpDown PosX_updown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Circle_Algo_/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Circle_Algo_
+{
+    public partial class Form1 : Form
+    {
+        // Center
+
+        public static int R = 10;   // Center >> ( 0 , R )
+        public static int Rx = 0;
+        public static int P = 1 - R;  // R
+        // Size
+        public static int x = 90;
+        public static int y = 80;
+        // Ends here the Size
+
+        // End Center
+        int Pos_X = 20; int Pos_Y = 60;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Pen pen = new Pen(Color.BlueViolet, 2.0f);
+
+            for (int i = 0; i < 7; i++)
+            {
+                while (Rx <= R)
+                {
+                    if (P >= 0)
+                    {
+                        ++Rx;
+                        --R;
+                        P = P + 2 * Rx - 2 * R + 5;
+                        graphics.DrawArc(pen, Pos_X, Pos_Y, x, y, Rx, R);
+                    }
+                    else
+                    {
+                        ++Rx;
+                        P = P + 2 * Rx + 3;
+                        graphics.DrawArc(pen, Pos_X, Pos_Y, x, y, Rx, R);
+                    }
+                }
+                R += R;
+                Rx = R;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            R = int.Parse(R_updown.Value.ToString());
+            Pos_X = int.Parse(PosX_updown.Value.ToString());
+            Pos_Y = int.Parse(PosY_updown.Value.ToString());
+            //x = int.Parse(SizeX_updown.Value.ToString());
+            //y = int.Parse(SizeY_updown.Value.ToString());
+            x = int.Parse(R_updown.Value.ToString());
+            y = int.Parse(R_updown.Value.ToString());
+            this.Invalidate();
+
+        }
+    }
+}
